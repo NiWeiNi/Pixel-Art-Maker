d6567f2cf6f913de3875ad47e90034a2bf3e9537
Wei <weinini007@gmail.com>
2018-03-09 19:57:36 +0100

    Add variables to js

--- js/designs.js
+++ js/designs.js
@@ -1,9 +1,21 @@
 // Select color input
+
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+    let height = document.querySelector('#inputHeight').value;
+    let weight = document.querySelector('inputWeight').value;
+    let table = document.querySelector('#pixelCanvas').value;
+
+    for (let i = 0; i < width; i++) {
+        
+        table += "<tr>";
+        console.log(table);
+        
+    }
+
 
 // Your code goes here!
 
