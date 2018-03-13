6ec0cdb64d6eb2bd793407096eafe56bbd663fa0
Wei <weinini007@gmail.com>
2018-03-13 19:16:02 +0100

    Add delete any previous table

--- js/designs.js
+++ js/designs.js
@@ -1,22 +1,57 @@
 // Select color input
 
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
-    let height = document.querySelector('#inputHeight').value;
-    let weight = document.querySelector('inputWeight').value;
-    let table = document.querySelector('#pixelCanvas').value;
 
-    for (let i = 0; i < width; i++) {
+    // Select size input
+    let height = document.querySelector('#inputHeight').value;
+    let width = document.querySelector('#inputWeight').value;
+
+    // Select table
+    let table = document.querySelector('#pixelCanvas');
+
+    // Delete previous table
+    table.remove();
+
+    // Create new table
+    table = document.createElement('table')
+
+    // Add previous ID
+    table.id = 'pixelCanvas';
+
+    // Append table to body
+    document.querySelector('body').append(table);
+
+    // Create rows of the table
+    for (let w = 0; w < width; w++) {
         
-        table += "<tr>";
-        console.log(table);
-        
+        let tr = document.createElement('tr');
+        table.appendChild(tr);
+
+        // Create columns of the table
+        for (let c = 0; c < height; c++) {
+
+            let td = document.createElement('td');
+            table.lastChild.append(td);
+        }
     }
 
+    // Select cells table
+    let cells = table.querySelectorAll('td');
 
-// Your code goes here!
+    // Store color
+    let color = document.querySelector('input[type="color"]').value;
 
+    // Add EventListener on cells
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].addEventListener('click', function() {
+            // Set background color to color picked
+            cells.style.background = color;
+        });
+    }
 }
+
+// When size is submitted by the user, call makeGrid()
+ document.querySelector('input[type="submit"]').addEventListener('click', (function(event) {
+    event.preventDefault();
+    makeGrid();
+ }));
